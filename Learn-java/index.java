25cba564bf511c68dc096e33580580df54101f3f
David <jodasos03@gmail.com>
2022-09-20 22:44:11 +0200

    upload proyect

---
+++ Learn-java/index.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+
+    public static void main(String args[]) {
+
+        System.out.println("Hola Este es mi Primer Programa");
+
+        System.out.println("Vamos A seguir estudiando Java");
+
+    }
+}
